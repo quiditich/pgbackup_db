8435d04294c147f150332319aa7dd92e18a4fada
quiditich <a.manzareanu@yahoo.com>
2019-12-18 11:12:26 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/quiditich/pgbackup',
+    url='https://github.com/quiditich/pgbackup_db',
     packages=find_packages('src')
 )
